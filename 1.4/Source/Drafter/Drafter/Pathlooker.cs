8f7998da17d57e7295cb6d6ed9afa3bddf0f46a5
spationaut <125911680+spationaut@users.noreply.github.com>
2023-04-09 14:48:34 +0200

    file: create entire mod
    Valid commit, I mean Visual Studio creates dozens of stupid dummy files, what do you want me to do about it?

---
+++ 1.4/Source/Drafter/Drafter/Pathlooker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using HarmonyLib;
+
+namespace Drafter
+{
+    public class Pathlooker : Mod
+    {
+        public Pathlooker(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony("Drafter");
+            harmony.Patch(AccessTools.Method(typeof(Pawn_RotationTracker), nameof(Pawn_RotationTracker.UpdateRotation)), null, null, new HarmonyMethod(typeof(Pawn_RotationTracker_UpdateRotation), "Transpiler"));
+        }
+
+        public static class Pawn_RotationTracker_UpdateRotation
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> instructionList = new List<CodeInstruction>(instructions);
+
+                for (var i = 0; i < instructionList.Count; i++)
+                {
+                    if (instructionList[i].opcode == OpCodes.Ldarg_0)
+                    {
+                        if (instructionList[i + 1].opcode == OpCodes.Ldfld)
+                        {
+                            if (instructionList[i + 2].opcode == OpCodes.Callvirt)
+                            {
+                                MethodInfo draftedInfo = AccessTools.PropertyGetter(typeof(Pawn), nameof(Pawn.Drafted));
+                                MethodInfo instructionInfo = instructionList[i + 2].operand as MethodInfo;
+
+                                if (instructionInfo != null && instructionInfo == draftedInfo)
+                                {
+                                    var ret = new CodeInstruction(OpCodes.Ret);
+                                    ret.labels.Add(instructionList[i].labels[0]);
+                                    yield return ret;
+                                    yield break;
+                                }
+                            }
+                        }
+                    }
+                    yield return new CodeInstruction(instructionList[i]);
+                }
+            }
+        }
+    }
+}
